e3a52a38772b502b4a84fa468ab6f5552b804a3d
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-01-17 01:10:44 -0300

    chore: project structure

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      firaCode: ["Fira Code", "monospace"],
+    },
+    extend: {
+      colors: {
+        brand: {
+          dark: "#0F0D13",
+          "player-bg": "#2A2141",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
