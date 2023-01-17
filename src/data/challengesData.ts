199766baa6113184fa133d80863232aa26c66a46
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-01-17 01:16:13 -0300

    feat: removing mocking data

--- src/data/challengesData.ts
+++ src/data/challengesData.ts
@@ -13,25 +13,4 @@ export const challenges = [
     figmaUrl: "https://www.figma.com/community/file/1195050984449538256",
     imageSrc: "assets/challenges/week2.png",
   },
-  {
-    week: 3,
-    title: "Music Player",
-    path: "/player",
-    figmaUrl: "https://www.figma.com/community/file/1195050524500542670",
-    imageSrc: "assets/challenges/week1.png",
-  },
-  {
-    week: 4,
-    title: "Music Player",
-    path: "/player",
-    figmaUrl: "https://www.figma.com/community/file/1195050524500542670",
-    imageSrc: "assets/challenges/week1.png",
-  },
-  {
-    week: 5,
-    title: "Music Player",
-    path: "/player",
-    figmaUrl: "https://www.figma.com/community/file/1195050524500542670",
-    imageSrc: "assets/challenges/week1.png",
-  },
 ]
