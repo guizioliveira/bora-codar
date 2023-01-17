8182a3e2df97850756786ad45ea0ccbace8520ca
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-01-17 16:06:51 -0300

    feat: create component `VariantOptions`

---
+++ src/components/Player/VariantOptions.tsx
@@ -0,0 +1,39 @@
+import { useMusicPlayer } from "../../hooks/useMusicPlayer"
+
+export default function VariantOptions() {
+  const { variant, setVariant } = useMusicPlayer()
+  return (
+    <div className="flex h-3 w-full justify-between bg-brand-player-bg py-8 px-8 md:w-96 md:rounded-t-[9.6px]">
+      <label className="flex items-center gap-2 text-white">
+        <input
+          type="radio"
+          value="full"
+          name="full"
+          checked={variant === "full"}
+          onChange={(checked) => setVariant("full")}
+        />
+        Full
+      </label>
+      <label className="flex items-center gap-2 text-white">
+        <input
+          type="radio"
+          value="short"
+          name="short"
+          checked={variant === "short"}
+          onChange={(checked) => setVariant("short")}
+        />
+        Short
+      </label>
+      <label className="flex items-center gap-2 text-white">
+        <input
+          type="radio"
+          value="minimal"
+          name="minimal"
+          checked={variant === "minimal"}
+          onChange={(checked) => setVariant("minimal")}
+        />
+        Minimal
+      </label>
+    </div>
+  )
+}
