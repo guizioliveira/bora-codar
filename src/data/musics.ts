9f9a29df2ed6fab8321ae8e58c59d7b08d5ee532
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-01-17 16:04:53 -0300

    chore: add ``music` mock data

---
+++ src/data/musics.ts
@@ -0,0 +1,16 @@
+export const musics = [
+  {
+    id: 0,
+    title: "Acorda Devinho",
+    artist: "Banda Rocketseat",
+    duration: 200,
+    albumCover: "assets/albumCover/1.png",
+  },
+  {
+    id: 1,
+    title: "High Hopes",
+    artist: "Panic! At The Disco",
+    duration: 190,
+    albumCover: "assets/albumCover/2.jpg",
+  },
+]
