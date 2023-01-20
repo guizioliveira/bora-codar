fc9dffc2768d3305b688b937f73643dc6df93bb3
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-01-20 00:16:50 -0300

    feat: adding new `Button`

--- src/components/ButtonVariants/index.tsx
+++ src/components/ButtonVariants/index.tsx
@@ -1,42 +1,43 @@
 import React, { useState } from "react"
 import { TestArea } from "./TestArea"
-import { CaretLeft, CaretRight, X } from "phosphor-react"
+import { X } from "phosphor-react"
 import ButtonTable from "./ButtonTable"
+import Button from "../Ui/Button"
 
 export function ButtonStylePage() {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   return (
-    <div className="relative h-[calc(100vh-80px)] w-full overflow-hidden bg-[#0b0911] pt-24">
-      <div className="m-auto flex w-[958px] flex-col">
-        <h1 className="mb-2 w-full text-left font-inter text-[32px] font-bold leading-[32px] text-white">
+    <div className="relative min-h-[calc(100vh-80px)] w-full overflow-hidden bg-[#0b0911] pt-12 md:pt-24">
+      <div className="m-auto flex w-full flex-col px-5 md:w-[958px] md:p-0">
+        <h1 className="mb-8 w-full text-left font-inter text-[32px] font-bold leading-[32px] text-white md:mb-2">
           Tipos de botão
         </h1>
-        <p className="mb-14 font-sans text-xl leading-[23px] text-white opacity-[0.67]">
+        <p className="font-sans text-xl text-white opacity-[0.67] md:leading-[23px]">
           Dentro de um layout, botões servem para destacar ações importantes a
-          serem tomadas. <br />
+          serem tomadas.
+        </p>
+        <p className="font-sans text-xl text-white opacity-[0.67] md:leading-[23px]">
           Acompanhe abaixo um exemplo de tipos e propriedades.
         </p>
-        <ButtonTable />
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className={`${
-            isOpen ? "right-[406px]" : "right-0"
-          } absolute top-1/2 flex h-16 w-8 items-center justify-center gap-2 rounded-l-md bg-[#130F1E] transition-all duration-300`}
-        >
-          {isOpen ? (
-            <CaretRight weight="bold" size={24} color={"#fff"} />
-          ) : (
-            <CaretLeft weight="bold" size={24} color={"#fff"} />
-          )}
-        </button>
+        <Button
+          className="my-7 w-2/3  md:w-1/3"
+          variant="primary"
+          label="Bora testar!"
+          onClick={() => {
+            setIsOpen(true)
+          }}
+        />
+        <div className="mb-8">
+          <ButtonTable />
+        </div>
         <div
           className={`${
             isOpen ? "translate-x-0" : "translate-x-full"
-          } absolute top-0 right-0 h-[calc(100vh-80px)] w-full bg-[#130F1E] px-11 pt-[107px] transition-all duration-300 md:w-[406px]`}
+          } fixed top-0 right-0 h-screen w-full bg-[#130F1E] px-11 pt-[107px] transition-all duration-300 md:w-[406px]`}
         >
           <button
             onClick={() => setIsOpen(false)}
-            className="absolute right-8 top-5 block text-white md:hidden"
+            className="absolute right-8 top-5 text-white"
           >
             <X size={32} />
           </button>
