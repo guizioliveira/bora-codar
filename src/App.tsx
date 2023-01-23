24a390bddc8cd3f489cd266fa2607beaa85c5cfa
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-01-23 18:01:24 -0300

    fix: adjusting body height

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,11 @@ import { getChallenges } from "@services"
 export default function App() {
   const challenges = getChallenges()
   return (
-    <div className=" bg-[#121214]">
-      <div className="m-auto h-[calc(100vh-80px)] w-full p-7 md:container">
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
-          {challenges.map((challenge) => (
-            <ChallengeCard key={challenge.path} challenge={challenge} />
-          ))}
-        </div>
+    <div className="m-auto min-h-[calc(100vh-80px)] w-full p-7 md:container">
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+        {challenges.map((challenge) => (
+          <ChallengeCard key={challenge.path} challenge={challenge} />
+        ))}
       </div>
     </div>
   )
