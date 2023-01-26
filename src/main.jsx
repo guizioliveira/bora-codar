6482e78fdca29539230bdd4086cbea0245a949b4
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-01-26 02:17:02 -0300

    fix: adjusting path mapping

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Routers from "../Routes/routes"
-import { Header } from "./components/Header"
+import Routers from "../routes/routes"
+import { Header } from "@components/Header"
 
 import "./styles/global.css"
 
