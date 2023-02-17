a43017e4922964d4a1eb4d50fcf2c709ba1f7579
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-02-16 21:59:46 -0300

    fix(chat): placehoder text color

--- src/projects/Chat/MessageForm.tsx
+++ src/projects/Chat/MessageForm.tsx
@@ -20,7 +20,7 @@ export function MessageForm() {
       className="mt-14 flex w-full rounded-full bg-[#282843]"
     >
       <input
-        className="w-[calc(100%-58px)] rounded-full bg-[#282843] py-[19px] px-6 text-xs leading-[14px] text-[#E1E1E6] outline-none placeholder:text-[#E1E1E6]"
+        className="placeholder:text-[#E1E1E6][.5] w-[calc(100%-58px)] rounded-full bg-[#282843] py-[19px] px-6 text-xs leading-[14px] text-[#E1E1E6] outline-none"
         type="text"
         value={currentMessage}
         onChange={(event) => setCurrentMessage(event.target.value.toString())}
