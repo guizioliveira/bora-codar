7428830f14417531df245a8784e64423d83aff34
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-04-02 14:38:01 -0300

    feat: improving data fetch calls with cache

--- src/services/challengesData.ts
+++ src/services/challengesData.ts
@@ -1,60 +1,6 @@
 import { gql, useQuery } from "@apollo/client"
 import { challengeType } from "@type/challengesTypes"
 
-// const ASSETS_PATH = "assets/challenges"
-
-// const challenges: challengeType = [
-//   {
-//     week: 1,
-//     title: "Music Player",
-//     path: `player`,
-//     figmaUrl: "https://www.figma.com/community/file/1195050524500542670",
-//     imageSrc: `${ASSETS_PATH}/week1.png`,
-//   },
-//   {
-//     week: 2,
-//     title: "Product Card",
-//     path: `product-card`,
-//     figmaUrl: "https://www.figma.com/community/file/1195050984449538256",
-//     imageSrc: `${ASSETS_PATH}/week2.png`,
-//   },
-//   {
-//     week: 3,
-//     title: "Design System - Buttons",
-//     path: `buttons`,
-//     figmaUrl: "https://www.figma.com/community/file/1197534710257750520",
-//     imageSrc: `${ASSETS_PATH}/week3.png`,
-//   },
-//   {
-//     week: 4,
-//     title: "Chat layout",
-//     path: `chat`,
-//     figmaUrl: "https://www.figma.com/community/file/1200070743637495660",
-//     imageSrc: `${ASSETS_PATH}/week4.png`,
-//   },
-//   {
-//     week: 5,
-//     title: "Calculator",
-//     path: `calculator`,
-//     figmaUrl: "https://www.figma.com/community/file/1202607074523509182",
-//     imageSrc: `${ASSETS_PATH}/week5.png`,
-//   },
-//   {
-//     week: 6,
-//     title: "Boarding Pass",
-//     path: `boarding-pass`,
-//     figmaUrl: "https://www.figma.com/community/file/1205146101173113980",
-//     imageSrc: `${ASSETS_PATH}/week6.png`,
-//   },
-//   {
-//     week: 7,
-//     title: "Dashboard",
-//     path: `dashboard`,
-//     figmaUrl: "https://www.figma.com/community/file/1210217615683203825",
-//     imageSrc: `${ASSETS_PATH}/week7.png`,
-//   },
-// ]
-
 const GET_ALL_CHALLENGES_QUERY = gql`
   query {
     challenges(orderBy: week_DESC, stage: PUBLISHED) {
@@ -70,9 +16,11 @@ const GET_ALL_CHALLENGES_QUERY = gql`
 `
 
 export function getChallenges() {
-  const { data } = useQuery<{ challenges: Array<challengeType> }>(
-    GET_ALL_CHALLENGES_QUERY
-  )
+  const { data, loading, error } = useQuery<{
+    challenges: Array<challengeType>
+  }>(GET_ALL_CHALLENGES_QUERY, {
+    fetchPolicy: "cache-first",
+  })
 
-  return data
+  return { data, error, loading }
 }
