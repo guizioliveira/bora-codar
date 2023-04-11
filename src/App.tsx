8a1a8454fb39f6db803b7f5f66793fd4c3eb032f
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-04-11 20:55:09 -0300

    feat: new `ChallengeCardSkeleton` component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Header } from "@components/Header"
 import ChallengeCard from "@components/ChallengeCard"
 import { getChallenges } from "@services"
+import ChallengeCardSkeleton from "@components/ChallengeCardSkeleton"
 
 export default function App() {
   const { data, loading } = getChallenges()
@@ -9,17 +10,17 @@ export default function App() {
     <>
       <Header />
       <div className="m-auto w-full p-4 md:container md:p-7">
-        {loading ? (
-          <div className="animate-pulse text-center text-3xl font-bold text-white">
-            Loading...
-          </div>
-        ) : (
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
-            {data.challenges.map((challenge) => (
-              <ChallengeCard key={challenge.path} challenge={challenge} />
-            ))}
-          </div>
-        )}
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+          {loading ? (
+            <ChallengeCardSkeleton numberOfCards={6} />
+          ) : (
+            <>
+              {data.challenges.map((challenge) => (
+                <ChallengeCard key={challenge.path} challenge={challenge} />
+              ))}
+            </>
+          )}
+        </div>
       </div>
     </>
   )
